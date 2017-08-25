5158bb903c916588deb2bc074d7006428e1d57eb
Tomek <prysak.tomasz@gmail.com>
2017-08-25 10:39:11 +0200

    Login Controler - do testów

---
+++ src/app/controller/LoginController.java
@@ -0,0 +1,94 @@
+package app.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import app.database.DBConnector;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class LoginController {
+	
+	@FXML
+    private TextField tf_imie;
+
+    @FXML
+    private PasswordField tf_nazwisko;
+
+    @FXML
+    private Button btn_login;
+    
+    @FXML
+    private CheckBox cb_db;
+
+    @FXML
+    private CheckBox cb_python;
+
+    @FXML
+    private CheckBox cb_fe;
+    
+    @FXML
+    private CheckBox cb_java;
+    
+    @FXML
+    private CheckBox cb_spring;
+    
+    @FXML
+    private VBox zakresPytan;
+    
+    public DBConnector;
+    String logowanie;
+	
+    @FXML
+    void loginAction(MouseEvent event) throws ClassNotFoundException, SQLException, IOException {
+    	
+    	Connection conn1 = db.Connection();
+    	
+    	Statement stat = conn1.createStatement();
+    	
+    	ResultSet rs = stat.executeQuery("select * from users where imie = '" + tf_imie.getText() + "' and nazwisko = '" + tf_nazwisko.getText() + "';");
+    
+    	if(rs.next()){
+    		
+    		System.out.println(logowanie);
+    		
+    		zakresPytan.setDisable(false);
+    		
+    	}else{
+    		Alert a = new Alert(AlertType.WARNING);
+    		a.setContentText("Poda³eœ b³êdny login lub has³o");
+    		a.setTitle("B³¹d");
+    		a.setHeaderText("UWAGA!");
+    		a.showAndWait();
+    	}
+    	
+    }
+    
+    @FXML
+    void graAction(MouseEvent event) throws ClassNotFoundException, IOException {
+    	
+		Stage stageGame = new Stage();
+		Parent parent = (Parent) FXMLLoader.load(getClass().getResource("/app/view/DataBaseView.fxml"));
+		Scene sceneDB = new Scene(parent);
+		stageDB.setScene(sceneDB);
+		stageDB.setTitle("DataBaseView");
+		stageDB.setResizable(false);
+		stageDB.show();
+    	
+	}
+    
+}
